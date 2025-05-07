4b2f05dc1c275de92776e195c79e7480005d3e24
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-07 11:28:15 -0300

    feat(services): Added CRUD in Cliente Service

--- src/main/java/com/felipelohan/mvdesafio/services/ClienteService.java
+++ src/main/java/com/felipelohan/mvdesafio/services/ClienteService.java
@@ -2,16 +2,32 @@ package com.felipelohan.mvdesafio.services;
 
 import com.felipelohan.mvdesafio.entities.Cliente;
 import com.felipelohan.mvdesafio.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ClienteService {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+    private final ClienteRepository clienteRepository;
 
     public Cliente salvar(Cliente cliente) {
         return clienteRepository.save(cliente);
     }
+
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
 }
+
