337c3d7b3871d1e73c39963fe0dddfd9edbfb66c
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-07 09:33:29 -0300

    First Commit

---
+++ src/main/java/com/felipelohan/mvdesafio/MvdesafioApplication.java
@@ -0,0 +1,13 @@
+package com.felipelohan.mvdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvdesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvdesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipelohan/mvdesafio/MvdesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipelohan.mvdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvdesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
