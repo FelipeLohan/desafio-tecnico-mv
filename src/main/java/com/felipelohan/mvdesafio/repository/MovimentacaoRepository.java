0ffff0116616ddb2270c676af6ff6b7deb3d096e
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-07 19:33:54 -0300

    feat(repository): Added findByClientId

--- src/main/java/com/felipelohan/mvdesafio/repository/MovimentacaoRepository.java
+++ src/main/java/com/felipelohan/mvdesafio/repository/MovimentacaoRepository.java
@@ -3,5 +3,8 @@ package com.felipelohan.mvdesafio.repository;
 import com.felipelohan.mvdesafio.entities.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+    List<Movimentacao> findByClienteId(Long clienteId);
 }
