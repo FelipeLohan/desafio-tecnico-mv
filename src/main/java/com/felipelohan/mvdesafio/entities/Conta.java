9ea82da3a21ca0ed0ec77d29bec913524dbd62a6
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-07 10:50:36 -0300

    feat(entities): Added Movimentacao entitie

---
+++ src/main/java/com/felipelohan/mvdesafio/entities/Conta.java
@@ -0,0 +1,37 @@
+package com.felipelohan.mvdesafio.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero_conta", nullable = false, length = 20)
+    private String numeroConta;
+
+    @Column(name = "agencia", nullable = false, length = 10)
+    private String agencia;
+
+    @Column(name = "instituicao_financeira", nullable = false, length = 100)
+    private String instituicaoFinanceira;
+
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo = true;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+}
